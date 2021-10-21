b95a1fbbb3d629af3dc2d2a985022735c4a9e60c
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-10-21 16:16:56 +0530

    Update lambda_basic_rest.py

--- lambda_basic_rest.py
+++ lambda_basic_rest.py
@@ -13,13 +13,21 @@ def lambda_handler(event, context):
     logger.info(event)
     
     # POST method - it carries a payload as json content
-    body = event["body"] # read data from api gateway
-    data = json.loads(body) # parsing json string to json object
-    logger.info("Product name is " + data["name"])
+    if (event["httpMethod"] == 'GET'):
+        return {
+            'statusCode': 404,
+            'body':  json.dumps({'error': 'resource not found'}),
+            "headers": { "X-ENCRYPTED": "true"}
+        }
+    else:
+        # POST Method, data coming from the http-client 
+        body = event["body"] # read data from api gateway
+        data = json.loads(body) # parsing json string to json object
+        logger.info("Product name is " + data["name"])
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps({"result": "success", "productName": data["name"]}),
-        "headers": { "X-ENCRYPTED": "true", "content-type": "application/json"},
-
-    }
+        return {
+            'statusCode': 200,
+            'body': json.dumps({"result": "success", "productName": data["name"]}),
+            "headers": { "X-ENCRYPTED": "true", "content-type": "application/json"},
+    
+        }
