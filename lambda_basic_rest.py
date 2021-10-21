7224fb169b4e55a856b6adff6f00054417348cea
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-10-21 15:42:47 +0530

    Update lambda_basic_rest.py

--- lambda_basic_rest.py
+++ lambda_basic_rest.py
@@ -6,18 +6,20 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     # TODO implement
+    print("My REST API")
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
     logger.info('## EVENT')
     logger.info(event)
     
-    body = event["body"]
-    data = json.loads(body)
+    # POST method - it carries a payload as json content
+    body = event["body"] # read data from api gateway
+    data = json.loads(body) # parsing json string to json object
     logger.info("Product name is " + data["name"])
     
     return {
         'statusCode': 200,
-        'body': json.dumps({"result": "success"}),
+        'body': json.dumps({"result": "success", "productName": data["name"]}),
         "headers": { "X-ENCRYPTED": "true", "content-type": "application/json"},
 
     }
